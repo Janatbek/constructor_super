60b19634ec24a0ad2d7e1936c46b1197ebe76b32
Janatbek Sharsheyev <janatbek@gmail.com>
2023-03-29 21:01:14 -0600

    adds person object

--- app.js
+++ app.js
@@ -33,7 +33,13 @@ function Car(year, model, color, brand) {
 	this.model = model;
 	this.color = color;
 	this.brand = brand;
-} // Object instance.
+    this.wheels = 4;
+    this.steeringWheelLocation = 'left';
+    this.drive = function() {
+        console.log('driving')
+    }
+}
+// Object instance.
 // window is here, it's global.
 // Factory function
 function car(year, model, color, brand) {
@@ -44,6 +50,11 @@ function car(year, model, color, brand) {
 console.log(car(2015, 'avalon', 'white', 'toyota'));
 const ford = new Car(2023, 'F150', 'blue', 'Ford'); // give me new instance of you.
 const tesla = new Car(2020, 'Model X', 'white', 'Tesle'); // give me new instance of you.
+
+tesla.engine = 'electric';
+tesla.wheels = 3;
+tesla.steeringWheelLocation = 'right';
+ford.engine = 'combustion';
 console.log(ford);
 console.log(tesla);
 
@@ -86,3 +97,34 @@ What happens is when you use the new keyword on a function, it creates a new ins
 /*
 The this keyword in JavaScript refers to the instance of an object that a function is bound to.
 */
+
+function Person(gender, nationality, height, eyesColor) {
+    this.eyesColor = eyesColor;
+    this.nationality = nationality;
+    this.height = height;
+    this.gender = gender;
+    this.legs = 2;
+    this.hands = 2;
+    this.head = 1;
+    this.neck = 1;
+    this.stomack = 1;
+    this.eat = function(meal) {
+        console.log(`I am eating ${meal}`);
+    }
+    this.run = function(distance, unit) {
+        console.log(`I am running ${distance} ${unit} `)
+    }
+    this.drink = function(drink) {
+        console.log(`I am drinking ${drink}`);
+    }
+}
+
+const babyBoy = new Person('male', 'US', '50cm', 'black');
+const babyGirl = new Person('female', 'US', '49cm', 'black');
+console.log(babyBoy)
+console.log(babyBoy.drink('milk'))
+babyBoy.age = 3;
+babyBoy.run(100, 'ft')
+console.log(babyGirl);
+babyGirl.age = 5;
+babyGirl.eat('meat');
